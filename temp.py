c1b7ea5867ee9814e1690abd3c86fecdb9f21319
shiou <df404254@gmail.com>
2022-07-19 14:29:32 +0800

    read image

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import cv2
+img2 = cv2.imread('images/img2.jpg')
+cv2.imshow('My Image',img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
