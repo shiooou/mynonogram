7970351345496e049f0ac562ebfcb29e93d11f6b
shiou <df404254@gmail.com>
2022-07-19 15:55:27 +0800

    讀取檔案中圖片

--- temp.py
+++ temp.py
@@ -7,7 +7,26 @@ This is a temporary script file.
 
 import numpy as np
 import cv2
-img2 = cv2.imread('images/img2.jpg')
-cv2.imshow('My Image',img2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import os
+
+x
+def read_images(images):
+    array_of_images = []
+    for filename in os.listdir(r'./'+images):
+        img = cv2.imread(images + '/' + filename)
+        if img is not None:
+            array_of_images.append(img)
+        else:
+            print("Cannot read file {}".format(images + '/' + filename))
+        
+    return array_of_images
+
+if __name__ == "__main__":
+    
+    array_of_images = read_images('images')
+    
+    for image in array_of_images:
+        cv2.imshow("filename",image)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
+        
